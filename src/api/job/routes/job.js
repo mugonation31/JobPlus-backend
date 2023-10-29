7b45228e3229463edbeb707df867461382fcf7e0
Simba Mugomezi <simbadip@hotmail.co.uk>
2023-10-29 23:19:56 +0000

    wrote bespoke route, controller and service code

--- src/api/job/routes/job.js
+++ src/api/job/routes/job.js
@@ -1,9 +1,10 @@
-'use strict';
-
-/**
- * job router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::job.job');
+module.exports = {
+  routes: [
+    {
+      // Path defined with an URL parameter
+      method: "GET",
+      path: "/jobs",
+      handler: "job.find",
+    },
+  ],
+};
--- src/api/job/services/job.js
+++ src/api/job/services/job.js
@@ -1,9 +1,42 @@
-'use strict';
+"use strict";
 
 /**
  * job service
  */
+module.exports = ({ strapi }) => ({
+  async find(params) {
+    const { start = 0, limit = 10, ...rest } = params;
+    try {
+      const [entries, totalCount] = await Promise.all([
+        strapi.entityService.findMany("api::job.job", {
+          start,
+          limit,
+          ...rest,
+        }),
+        strapi.entityService.count("api::job.job", params),
+      ]);
 
-const { createCoreService } = require('@strapi/strapi').factories;
+      //Calculate pagination metadata
+      const totalPages = Math.ceil(totalCount / limit);
+      const currentPage = Math.min(totalPages, Math.max(1, start));
+      const hasPrevPage = currentPage > 1;
+      const hasNextPage = currentPage < totalPages;
 
-module.exports = createCoreService('api::job.job');
+      return {
+        entries,
+        meta: {
+          paginate: {
+            totalCount,
+            totalPages,
+            currentPage,
+            hasPrevPage,
+            hasNextPage,
+          },
+        },
+      };
+    } catch (error) {
+      strapi.log.error(error);
+      throw error;
+    }
+  },
+});
